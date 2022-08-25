c55b24c617d059bf6c8fb3497bfa73a9b31ae4de
firzamc01 <51307054+firzamc01@users.noreply.github.com>
2022-08-25 22:33:04 +0700

    aku kangen anindya
    persipan gun script dan pindah script

---
+++ Berna/Assets/Script/Gun/GunController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public static GunController gunController;
+
+    [Header("Gun Controller Required Variable atau Value")]
+    public Camera cam;
+    public Transform gunPivot;
+    public Animator anim;
+
+    [Header("Gun ID")]
+    [SerializeField]
+    [Tooltip("ID senjata nanti di panggil ke playerGun samakan nilai array yang di playerGun untuk swap weapon")]
+    [Range(0,4)]
+    public int id;
+
+    
+    [Header("Gun Idle")]
+    [SerializeField] float curIdleSpeed;
+    [SerializeField] float curIdlePower;
+    [SerializeField] float curSprintSpeed;
+    [SerializeField] float curSprintPower;
+    Vector3 pos;
+
+    [Header("Gun Sway")]
+    [SerializeField]
+    [Range(1, 5)]
+    float intensity;
+    [SerializeField]
+    [Range(5, 15)]
+    float smooth;
+    Quaternion originRotPoint;
+
+    [Header("Gun Aiming")]
+    [SerializeField] Vector3 defaultPos;
+    [SerializeField] Vector3 aimingPos;
+    [Tooltip("kecepatan pindah dari defaultPos ke aimPos dan sebaliknya")]
+    [SerializeField] float movementAimPosSeed;
+    [Tooltip("zoom pada saat aim pke fov dari camera")]
+    [SerializeField] float aimFieldOfView;
+    [Tooltip("default fov pas lagi tidak aim")]
+    [SerializeField] float defaultFov;
+    [SerializeField] bool isAiming;
+
+    [Header("Gun Fire Rate")]
+    [SerializeField] float fireRate;
+    float delayShootTime;
+
+    private void Awake()
+    {
+        gunController = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void gunShoot() 
+    {
+        
+    }
+
+    void rayCastHitShoot() 
+    {
+        
+    }
+
+
+
+}
